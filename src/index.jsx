8c43228766bcb0d6dff59d8850feb9bf94a1ba52
Chard Peter <chardpeter10@gmail.com>
2024-12-11 07:07:21 +0000

    Delete src directory

--- src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOMClient from "react-dom/client";
-import { LoginPage } from "./screens/LoginPage";
-
-const app = document.getElementById("app");
-const root = ReactDOMClient.createRoot(app);
-root.render(<LoginPage />);
